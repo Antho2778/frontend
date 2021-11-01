5c7b66be0f3463f65e35a7cb695ab33624596400
Anthony Mordret <antho27540@gmail.com>
2021-11-01 18:27:01 +0100

    menu_extend avencement

--- src/components/ReviewExtend.jsx
+++ src/components/ReviewExtend.jsx
@@ -11,9 +11,9 @@ class ReviewExtend extends Component {
   } 
     render() {
       return (
-        <button className="square" onClick={() => this.props.onClick()}>
+        <li className="square" >
           {this.props.value}
-        </button>
+        </li>
       );
     }
 }
